c5ef01294cd2d255bdb376b010b36eb86b61a6c4
Samhayne <martin.walser@gmx.de>
2015-04-26 22:26:27 +0200

    Committing Project

---
+++ Source/FPSProject/FPSGameMode.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSProject.h"
+#include "FPSGameMode.h"
+//#include "FPSCharacter.h"
+
+AFPSGameMode::AFPSGameMode(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+	//DefaultPawnClass = AFPSCharacter::StaticClass();
+
+	// set default pawn class to our Blueprinted character	
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnObject(TEXT("Pawn'/Game/Blueprints/BP_FPSCharacter.BP_FPSCharacter_C'"));	
+	if (PlayerPawnObject.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnObject.Class;
+	}
+}
+
+// Note that engine version 4.3 changed the method's name to StartPlay(), because of this engine versions before 4.3, or older tutorials, use BeginPlay()
+void AFPSGameMode::StartPlay()
+{
+	Super::StartPlay();
+
+	StartMatch();
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("HELLO WORLD"));
+	}
+}
